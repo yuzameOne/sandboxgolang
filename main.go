8fc235f3aae485dd67bf1c8c124d7abbcbc842cf
yuzameOne <E.homayko@yandex.ru>
2020-12-24 07:11:44 +1000

    string split []slice

--- main.go
+++ main.go
@@ -1,31 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var stringLength int
 
 func main() {
+
 	s := "80.92.160.0-80.92.175.255"
 
 	// find full  string length
 
-	for i := 0; i < len(s); i++ {
+	// for i := 0; i < len(s); i++ {
 
-		stringLength = i
-		// fmt.Println(stringLength)
+	// 	stringLength = i
+	// 	// fmt.Println(stringLength)
+	// }
 
-	}
+	newString := strings.Split(s, ".")
 
-	for i := 0; i < len(s); i++ {
-		fmt.Printf("%v \n", s[i])
-		fmt.Printf("string data : %s \n", string(s[i]))
-		if s[i] == 10 {
-			lastNumber = s[1 : len(s)-1]
-			fmt.Printf(" print last  number string in loop : %s\n", lastNumber)
-		} else if s[i] == 45 {
-			copyString = s[0:i]
-			fmt.Printf(" print string in loop : %s\n", copyString)
-		}
-	}
+	substring := newString[3]
+
+	newsubstring := strings.Split(substring, "-")
+
+	// TODO variable newsubstring append in newString
+	// How copy slice 2 part??????
+
+	fmt.Println(substring)
+	fmt.Println(newsubstring)
+	fmt.Println(newString)
+
+	fmt.Println(newString[3])
+	fmt.Println(newString[6])
 
 }
