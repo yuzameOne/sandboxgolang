e3332b8cca36b0d56810a160cfd6d10e2b292a26
yuzameOne <E.homayko@yandex.ru>
2020-12-27 22:58:33 +1000

    added convert []slice to convert []int

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-var newFinish []string
+var newFinishSlice []string
+var convertStringSliceToInt = make([]int, len(newFinishSlice))
 
 func main() {
 	startString := "80.92.160.0-80.92.175.255"
@@ -16,8 +18,17 @@ func main() {
 
 	subStringRigth := strings.Split(string(newString[1]), ".")
 
-	newFinish = append(subStringLeft, subStringRigth...)
+	newFinishSlice = append(subStringLeft, subStringRigth...)
 
-	fmt.Println(newFinish)
+	fmt.Printf("type slice : %T , %v \n", newFinishSlice, newFinishSlice)
+
+	for _, val := range newFinishSlice {
+
+		value, _ := strconv.Atoi(val)
+
+		convertStringSliceToInt = append(convertStringSliceToInt, value)
+	}
+
+	fmt.Printf("type slice : %T , %v \n", convertStringSliceToInt, convertStringSliceToInt)
 
 }
