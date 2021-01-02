38cea91c546ff18cbdc98bfb114bba948a068d3c
L1bra <L1bradv86@gmail.com>
2021-01-02 16:26:45 +1000

    some

--- main.go
+++ main.go
@@ -31,20 +31,4 @@ func main() {
 
 	fmt.Printf("type slice : %T , %v \n", convertStringSliceToInt, convertStringSliceToInt)
 
-	// TODO equals 2 and 3 elements both  6 and 7
-	//  [80 92 160 0 80 92 175 255]
-
-	// k := 0
-	// j := 4
-
-	// for convertStringSliceToInt[k]; k <= 3; k++ {
-	// 	fmt.Println(convertStringSliceToInt[k])
-	// 	for convertStringSliceToInt[j]; j <= 7; j++ {
-	// 		fmt.Println(convertStringSliceToInt[j])
-	// 		if k == j {
-
-	// 		}
-	// 		break
-	// 	}
-	// }
 }
