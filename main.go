006f21b4c6d32fc3602ba816dd6cd46ce23b855a
yuzameOne <E.homayko@yandex.ru>
2021-01-26 02:47:03 +1000

    write check command line flag arguments

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
 
+	path, err := os.Getwd()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	var argOne string
+	flag.StringVar(&argOne, "pathtofile", "", "path to file ")
+
+	if argOne != "" {
+		fmt.Println("the first argument is missing")
+		os.Exit(3)
+	}
+
+	var argTwo string
+	flag.StringVar(&argTwo, "pathtosavefile", "", "path to file ")
+
+	flag.Parse()
+
+	if argTwo == "" {
+		argTwo = path + "/new_ip_" + argOne
+	}
+
+	fmt.Println(argOne)
+	fmt.Println(argTwo)
 }
