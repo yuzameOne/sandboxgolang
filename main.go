3ecbb9a6d227ee8d9f0662c94548cacb402eb18a
yuzameOne <E.homayko@yandex.ru>
2021-10-22 03:21:38 +1000

    update TODO

--- main.go
+++ main.go
@@ -1,47 +1,81 @@
 package main
 
 import (
-	"bufio"
-	"log"
-	"os"
-	"runtime"
-	"sync"
+	"fmt"
+	"strings"
 )
 
-var procsThreads int = runtime.GOMAXPROCS(0)
-var rangeIPStartFile []string
-var chanRangeIPStartFile = make(chan string)
-var countRangeRangeSliceIndex int
-var mu sync.Mutex
+var newCustomString strings.Builder
+var subSlice []string
 
 func main() {
+	// f, err := os.Open("example.txt")
 
-	file, err := os.OpenFile("example.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
-	if err != nil {
-		log.Fatalf("file not create : %s", err)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// defer f.Close()
+
+	// r := bufio.NewReader(f)
+
+	// for {
+	// 	line, err := r.ReadString('\n')
+
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	stringRange = append(stringRange, line)
+
+	// }
+
+	// fmt.Println(stringRange)
+
+	str := "5.100.67.0-5.100.67.255"
+
+	for index, value := range str {
+
+		if value == 46 || value == 45 {
+
+			subSlice = append(subSlice, newCustomString.String())
+			newCustomString.Reset()
+		}
+
+		if value != 46 && value != 45 {
+
+			newCustomString.WriteByte(str[index])
+
+			if len(str)-1 == index {
+				subSlice = append(subSlice, newCustomString.String())
+			}
+
+		}
+
 	}
+	fmt.Println(subSlice[1])
 
-	defer file.Close()
+	// index 2,3 and 6,7
+	// нужно в for сравнивать СТРОКИ index 2,3 and 6,7 если меньше  конвертировать в int, инкрементить
+	// и тут же конверитить в стороку и собирать это в нову строку StringBulder
 
-	scanner := bufio.NewScanner(file)
-
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-
-		rangeIPStartFile = append(rangeIPStartFile, scanner.Text())
-	}
-
-	for i := 0; i < len(rangeIPStartFile); i++ {
-
-		go func() {
-
-			mu.Lock()
-			countRangeRangeSliceIndex = countRangeRangeSliceIndex + 1
-			mu.Unlock()
-			
-			
-		}()
-	}
-
+	//  СУКА 2 фукции 3 канала)))))
 }
+
+// func removesPointAndDash(arrayRange []string) {
+
+// 	str := "5.100.67.0-5.100.67.255"
+
+// 	point := bytes.Index([]byte(str), []byte("-"))
+
+// 	leftString := str[:point]
+// 	rightString := str[point+1:]
+
+// 	fmt.Println(leftString)
+// 	fmt.Println(rightString)
+
+// 	// TODO
+// 	//  что надо  67.0 и  67.255 из стиринги в  инты сравить и обратно в строку  bytes.Buffer
+
+// 	// 21.10 14:11 стоял на смене и пришла мысль в голову , что последнее число  всегда будет в диапапозоне
+// 	//  от 0 до 255 (67.0). нужно просто создать Enum от 0 до 255 и подставить в цикле в строку  bytes.Buffer
+// }
